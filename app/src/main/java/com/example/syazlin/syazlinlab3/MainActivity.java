9027be08750abf6eb7d3baa757602396c7ba7d29
SYAZLIN <nsyazlinazwana@gmail.com>
2017-10-05 12:12:43 +0800

    Initial commit

---
+++ app/src/main/java/com/example/syazlin/syazlinlab3/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.syazlin.syazlinlab3;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        GridView gridView = (GridView) findViewById(R.id.gridView);
+        gridView.setAdapter(new ImageAdapter(this));
+
+        /**
+         * on click event for single gridview item
+         **/
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+             public void onItemClick(AdapterView<?> parent, View v, int position, long id){
+
+                //sending image id to FullScreenActivity
+                Intent i = new Intent(getApplicationContext(),FullImageActivity.class);
+
+                //passing array index
+                i.putExtra("id", position);
+                startActivity(i);
+            }
+        });
+    }
+}
